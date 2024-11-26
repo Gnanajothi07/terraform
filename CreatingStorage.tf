e3b777d32a4ed9511c24c766866904dbe2c73bb7
Gnanajothi07 <120785724+Gnanajothi07@users.noreply.github.com>
2024-11-26 12:57:57 +0530

    Create CreatingStorage.tf

---
+++ CreatingStorage.tf
@@ -0,0 +1,44 @@
+
+terraform {
+  required_version = ">= 0.12"
+}
+
+provider "aws" {}
+
+data "aws_caller_identity" "current" {}
+
+locals {
+  account_id    = data.aws_caller_identity.current.account_id
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+
+  bucket = "${local.account_id}-terraform-states"
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name         = "terraform-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
